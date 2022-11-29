a5629fcb89a602e3b6e4760b3b07f11802697044
dray91eu <dray91eu@gmail.com>
2022-11-29 11:32:31 -0800

    Pycodestyle validation

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
-
 def print_low_alpha():
     for c in 'abcdefghijklmnopqrstuvwxyz':
         print('{}'.format(c), end='')
-
-    if __name__ == '__main__':
-        print_low_alpha()
+if __name__ == '__main__':
+    print_low_alpha()
